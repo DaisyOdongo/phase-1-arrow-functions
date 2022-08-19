eb6e5faeecfed0874bef475aed67654b3321da82
DaisyOdongo <daisy.odongo@student.moringaschool.com>
2022-08-19 10:39:50 +0300

    commit tree 5fe6d9590ba3ad71ae43de3d27dd368236c2bf72

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(){
+    const x =2000;
+    const y = 100;{
+    return x /y;
+    }
+}
+divide(2000,100)
+    
+
+const square = x => x ** x;
+square(2,2);
+
+const add = (a, b) => a + b;
+add(3,4);
